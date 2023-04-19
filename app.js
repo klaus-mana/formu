69565718be9c8c9105cf529d3bf5efbb7aebb18c
klaus-mana <klausmana@arizona.edu>
2023-04-19 12:52:09 -0700

    Added the Route skeletons to the app

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 const express = require('express');
 const app     = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+app.use(express.static('./public'));
+
+const mongoose_url = '';
+
+//===========AUTH API ENDPOINTS===========//
+app.post('/auth/register', (req, res) => {
+
+});
+
+app.post('/auth/login', (req, res) => {
+
+});
+
+app.get('/auth/check', (req, res) => {
+
+});
+
+//===========CLIENT ROUTES===========//
+app.get('/', (req, res) => {
+
+});
+
+app.get('/explore', (req, res) => {
+
+});
+
+app.get('/formulas', (req, res) => {
+
+});
+
+app.get('/formulas/:id', (req, res) => {
+
+});
